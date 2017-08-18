4a5a4891fa798efad74b2411f8886bcc2348e6dc
May Aye Chann <cherrychirshateu@gmail.com>
2017-08-18 12:37:24 +0700

    WIP in validation

--- src/loginPage/loginPage.js
+++ src/loginPage/loginPage.js
@@ -10,7 +10,13 @@ const style = {
 class LoginPage extends Component {
     constructor(props){
         super(props);
-        
+        this.state = {
+           username: '',
+           usernameErrorText : '',
+           password: '',
+           passwordErrorText : '',
+           isDisabled : true
+        }
     }
     login(list) {
         console.log(list);
@@ -21,19 +27,37 @@ class LoginPage extends Component {
     componentDidMount() {
         this.login("it is form component did mount")
     }
+    validateUsername = (e, val) => { 
+        if(val === '') {
+            this.setState({ usernameErrorText: 'Username cant be blank', isDisabled: true})
+        }
+        else this.setState({ usernameErrorText: '', isDisabled: false})
+    }
+    validatePassword = (e, val) => { 
+        if(val === '') this.setState({ passwordErrorText: 'Password cant be blank', isDisabled: true}) 
+        else this.setState({ passwordErrorText: '', isDisabled: false})
+    }
+
     render() {
         return (
             <form className="container">
                 <div className="login-container">
                     <h4 style={{ color: '#616161'}}> Login Page  </h4>
                     <TextField
-                    hintText="Username"
-                    /><br /><br />
+                        hintText="Username"
+                        onChange= {this.validateUsername}
+                        errorText= {this.state.usernameErrorText}/><br /><br />
                     <TextField 
-                    hintText="Password"
-                    type="password"
-                    /><br /><br />
-                    <RaisedButton label="Login" onClick={this.handleClick.bind(this)} primary={true} style={style} />
+                        hintText="Password"
+                        type="password"
+                        onChange= {this.validatePassword}
+                        errorText= {this.state.passwordErrorText} /><br /><br />
+                    <RaisedButton 
+                        disabled={this.state.isDisabled}
+                        label="Login" 
+                        onClick={this.handleClick.bind(this)} 
+                        primary={true} 
+                        style={style} />
                 </div>
             </form>
             
