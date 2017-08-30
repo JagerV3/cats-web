f39c16eff6ba8482c7f9da51c684a5a850d63ab2
May Aye Chann <cherrychirshateu@gmail.com>
2017-08-30 18:59:56 +0700

    Moved route to app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router , Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import HomePage from './homePage/homePage.js';
+import LoginPage from './loginPage/loginPage.js';
+import RegisterPage from './registerPage/registerPage.js';
+import UserPage from './userPage/userPage.js';
+import SideBar from './controls/sideBar.js';
 import './App.css';
+
+const items = [
+  {
+      name: 'Login Page',
+      path: '/login'
+  },
+  {
+      name: 'Register Page',
+      path: '/register'
+  },
+  {
+      name: 'Dashboard',
+      path: '/login'
+  },
+  {
+      name: 'Task',
+      path: '/register'
+  },
+  {
+      name: 'Team',
+      path: '/register'
+  },
+  {
+      name: 'Project',
+      path: '/register'
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <MuiThemeProvider>
+            <div>
+                <SideBar items={items} />
+                <Route exact path="/" component={HomePage}/>
+                <Route path="/login" component={LoginPage}/>
+                <Route path="/register" component={RegisterPage}/>
+                <Route path="/user" component={UserPage} />
+            </div>
+        </MuiThemeProvider>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,55 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router , Route } from 'react-router-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import HomePage from './homePage/homePage.js';
-import LoginPage from './loginPage/loginPage.js';
-import RegisterPage from './registerPage/registerPage.js';
-import UserPage from './userPage/userPage.js';
 import App from './App';
-import SideBar from './controls/sideBar.js';
 import './index.css';
 
-const items = [
-    {
-        name: 'Login Page',
-        path: '/login'
-    },
-    {
-        name: 'Register Page',
-        path: '/register'
-    },
-    {
-        name: 'Dashboard',
-        path: '/login'
-    },
-    {
-        name: 'Task',
-        path: '/register'
-    },
-    {
-        name: 'Team',
-        path: '/register'
-    },
-    {
-        name: 'Project',
-        path: '/register'
-    }
-];
+
 ReactDOM.render(
-    <Router>
-        <MuiThemeProvider>
-            <div>
-                <SideBar items={items} />
-                <Route exact path="/" component={HomePage}/>
-                <Route path="/login" component={LoginPage}/>
-                <Route path="/register" component={RegisterPage}/>
-                <Route path="/user" component={UserPage} />
-                <Route path="/app" component={App} />
-            </div>
-        </MuiThemeProvider>
-    </Router>
+    <App/>
     , document.getElementById('root'));
 registerServiceWorker();
